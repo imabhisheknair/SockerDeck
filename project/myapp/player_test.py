cc6c25111919d3b8a735e918d955bd59458de1e7
imabhisheknair <abhisheknairav@gmail.com>
2023-01-08 23:45:39 +0530

    first commit

--- project/myapp/player_test.py
+++ project/myapp/player_test.py
@@ -22,7 +22,7 @@ from django.conf import settings
 
 def player_prediction(training_file,input_set):
     # importing data
-    data_path =  os.path.join(settings.BASE_DIR, 'data/data.csv')
+    data_path =  os.path.join(settings.BASE_DIR, 'data/output.csv')
     players = pd.read_csv(data_path)
     #players.head()
     # players.drop(['ID'], axis=1, inplace=True)
@@ -33,23 +33,21 @@ def player_prediction(training_file,input_set):
     # Model to find the best regression model
     data_path= training_file#'data.csv'
     model_data = pd.read_csv(data_path)
-    model_data.head()
-    # model_data.drop(['ID'], axis=1, inplace=True)
     model_data.drop(['overall'], axis=1, inplace=True)
+    model_data.drop('Unnamed: 0',axis=1, inplace=True)
     # model_data.drop(['player_positions'], axis=1, inplace=True)
 
-    #print(model_data['player_positions'].unique())
-    #model_data = pd.concat([model_data, pd.get_dummies(model_data['player_positions'], prefix='cut', drop_first=True)],axis=1)
-    #model_data.drop(['player_positions'], axis=1, inplace=True)
-    #model_data.head()
-    #cols = list(model_data)
-    #print(cols)
-    #model_data.plot.scatter(x='carat', y='price', s=1);
 
     target_name = 'value_eur'
     #scaler = sk.preprocessing
     robust_scaler = pre.RobustScaler()
-    X = model_data.drop('value_eur', axis=1)
+    X = model_data[['passing', 'dribbling', 'movement_reactions', 'power_shot_power', 
+    'mentality_composure', 'attacking_short_passing', 'skill_long_passing', 
+    'shooting', 'goalkeeping_diving', 'mentality_vision']]
+    print(list(X.columns))
+    print(list(model_data.columns))
+
+
     X = robust_scaler.fit_transform(X)
     y = model_data[target_name]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123) #20% test
@@ -63,49 +61,16 @@ def player_prediction(training_file,input_set):
                                                    y_true=y_test)
 
     #Linear Regression
-    linear_regression = LinearRegression()
-    linear_regression.fit(X_train, y_train)
-    models.loc['train_mse','MLR'] = mean_squared_error(y_pred=linear_regression.predict(X_train), y_true=y_train)
-    models.loc['test_mse','MLR'] = mean_squared_error(y_pred=linear_regression.predict(X_test), y_true=y_test)
+    gbr = GradientBoostingRegressor()
 
-    #Ridge Regreesion
-    # ridge_regression = Ridge()
-    # ridge_regression.fit(X_train, y_train)
-    # models.loc['train_mse','Ridge'] = mean_squared_error(y_pred=ridge_regression.predict(X_train), y_true=y_train)
-    # models.loc['test_mse','Ridge'] = mean_squared_error(y_pred=ridge_regression.predict(X_test), y_true=y_test)
+    gbr.fit(X_train, y_train)
+    models.loc['train_mse','MLR'] = mean_squared_error(y_pred=gbr.predict(X_train), y_true=y_train)
+    models.loc['test_mse','MLR'] = mean_squared_error(y_pred=gbr.predict(X_test), y_true=y_test)
 
 
-    #KNeighborsRegressor
-    # knn = KNeighborsRegressor(n_neighbors=10, weights='distance', metric='euclidean', n_jobs=-1) # k neighbours n =10
-    # knn.fit(X_train, y_train)
-    # models.loc['train_mse','KNN'] = mean_squared_error(y_pred=knn.predict(X_train), y_true=y_train)
-    # models.loc['test_mse','KNN'] = mean_squared_error(y_pred=knn.predict(X_test), y_true=y_test)
-
-    # Lasso Regression
-    # lasso = Lasso(alpha=0.1)
-    # lasso.fit(X_train, y_train)
-    # models.loc['train_mse','LASSO'] = mean_squared_error(y_pred=lasso.predict(X_train), y_true=y_train)
-    # models.loc['test_mse','LASSO'] = mean_squared_error(y_pred=lasso.predict(X_test), y_true=y_test)
-
-    # Random Forest Regressor
-    # clf_rf = RandomForestRegressor()
-    # clf_rf.fit(X_train, y_train)
-    # models.loc['train_mse','RF'] = mean_squared_error(y_pred=clf_rf.predict(X_train), y_true=y_train)
-    # models.loc['test_mse','RF'] = mean_squared_error(y_pred=clf_rf.predict(X_test), y_true=y_test)
-    #diamonds.head()
-
-    # rf_final = RandomForestRegressor(n_estimators =100, n_jobs=-1)
-    # rf_final.fit(X, y)
-
-
-    head_set=['age','height_cm','weight_kg','weak_foot','skill_moves','attacking_crossing',
-          'attacking_finishing','attacking_heading_accuracy','attacking_short_passing','attacking_volleys','skill_dribbling',
-          'skill_curve','skill_fk_accuracy','skill_long_passing','skill_ball_control','movement_acceleration',
-          'movement_sprint_speed','movement_agility','movement_reactions','movement_balance','power_shot_power',
-          'power_jumping','power_stamina','power_strength','power_long_shots','mentality_aggression','mentality_interceptions',
-          'mentality_positioning','mentality_vision','mentality_penalties','mentality_composure','defending_marking',
-          'defending_standing_tackle','defending_sliding_tackle','goalkeeping_diving','goalkeeping_handling','goalkeeping_kicking',
-          'goalkeeping_positioning','goalkeeping_reflexes'] #head
+    head_set=['passing', 'dribbling', 'movement_reactions', 'power_shot_power', 
+    'mentality_composure', 'attacking_short_passing', 'skill_long_passing', 
+    'shooting', 'goalkeeping_diving', 'mentality_vision'] #head
     #input_set=[32,170,72,94,4,4,88,95,70,92,88,97,93,94,92,96,91,84,93,95,95,86,68,75,68,94,48,40,94,94,75,96,33,37,26,6,11,15,14,8]
     new_data = collections.OrderedDict()
     i=0
@@ -114,33 +79,33 @@ def player_prediction(training_file,input_set):
         new_data[h]=input_set[i]
         i =i+1
     new_data = pd.Series(new_data).values.reshape(1,-1)
-    # print(linear_regression.predict(new_data))
-    estimate =  linear_regression.predict(new_data)[0]
+    # print(gbr.predict(new_data))
+    estimate =  gbr.predict(new_data)[0]
+    estimate = gbr.predict([[67, 72, 69, 75, 65, 70, 73, 78, 7, 67]])
     restimate = estimate / 1000000
     return restimate
     print('Done')
 
 def player_prediction2(training_file,input_set):
+    from sklearn.ensemble import GradientBoostingRegressor
+
     # importing data
     data_path= training_file#'data.csv'
     players = pd.read_csv(data_path)
+
     #players.head()
     # players.drop(['ID'], axis=1, inplace=True)
     players.drop(['value_eur'], axis=1, inplace=True)
 
-
-
-
-
-
     # Model to find the best regression model
     data_path= training_file#'data.csv'
     model_data = pd.read_csv(data_path)
+    print(list(model_data.columns))
+
     model_data.head()
     # model_data.drop(['ID'], axis=1, inplace=True)
-    model_data.drop(['value_eur'], axis=1, inplace=True)
+    # model_data.drop(['value_eur'], axis=1, inplace=True)
     # model_data.drop(['player_positions'], axis=1, inplace=True)
-
     #print(model_data['player_positions'].unique())
     #model_data = pd.concat([model_data, pd.get_dummies(model_data['player_positions'], prefix='cut', drop_first=True)],axis=1)
     #model_data.drop(['player_positions'], axis=1, inplace=True)
@@ -148,11 +113,19 @@ def player_prediction2(training_file,input_set):
     #cols = list(model_data)
     #print(cols)
     #model_data.plot.scatter(x='carat', y='price', s=1);
-
+   
     target_name = 'overall'
+    model_data.drop('Unnamed: 0',axis=1, inplace=True)
+
     #scaler = sk.preprocessing
     robust_scaler = pre.RobustScaler()
-    X = model_data.drop('overall', axis=1)
+    # X = model_data.drop('overall', axis=1)
+    X = model_data[['passing', 'dribbling', 'movement_reactions', 'power_shot_power', 
+    'mentality_composure', 'attacking_short_passing', 'skill_long_passing', 
+    'shooting', 'goalkeeping_diving', 'mentality_vision']]
+    y = model_data.overall
+    print(list(X.columns))
+
     X = robust_scaler.fit_transform(X)
     y = model_data[target_name]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
@@ -165,50 +138,13 @@ def player_prediction2(training_file,input_set):
     models.loc['test_mse','NULL'] = mean_squared_error(y_pred=np.repeat(y_pred_null, y_test.size),
                                                    y_true=y_test)
 
-    #Linear Regression
-    # linear_regression = LinearRegression()
-    # linear_regression.fit(X_train, y_train)
-    # models.loc['train_mse','MLR'] = mean_squared_error(y_pred=linear_regression.predict(X_train), y_true=y_train)
-    # models.loc['test_mse','MLR'] = mean_squared_error(y_pred=linear_regression.predict(X_test), y_true=y_test)
-
-    #Ridge Regression
-    ridge_regression = Ridge()
-    ridge_regression.fit(X_train, y_train)
-    models.loc['train_mse','Ridge'] = mean_squared_error(y_pred=ridge_regression.predict(X_train), y_true=y_train)
-    models.loc['test_mse','Ridge'] = mean_squared_error(y_pred=ridge_regression.predict(X_test), y_true=y_test)
-
-
-    # KNeighborsRegressor
-    # knn = KNeighborsRegressor(n_neighbors=10, weights='distance', metric='euclidean', n_jobs=-1)
-    # knn.fit(X_train, y_train)
-    # models.loc['train_mse','KNN'] = mean_squared_error(y_pred=knn.predict(X_train), y_true=y_train)
-    # models.loc['test_mse','KNN'] = mean_squared_error(y_pred=knn.predict(X_test), y_true=y_test)
-
-    # Lasso Regression/
-    # lasso = Lasso(alpha=0.1)
-    # lasso.fit(X_train, y_train)
-    # models.loc['train_mse','LASSO'] = mean_squared_error(y_pred=lasso.predict(X_train), y_true=y_train)
-    # models.loc['test_mse','LASSO'] = mean_squared_error(y_pred=lasso.predict(X_test), y_true=y_test)
-
-    # Random Forest Regressor
-    # clf_rf = RandomForestRegressor()
-    # clf_rf.fit(X_train, y_train)
-    # models.loc['train_mse','RF'] = mean_squared_error(y_pred=clf_rf.predict(X_train), y_true=y_train)
-    # models.loc['test_mse','RF'] = mean_squared_error(y_pred=clf_rf.predict(X_test), y_true=y_test)
-    # #diamonds.head()
-
-    # rf_final = RandomForestRegressor(n_estimators =100, n_jobs=-1)
-    # rf_final.fit(X, y)
-
-
-    head_set=['age','height_cm','weight_kg','weak_foot','skill_moves','attacking_crossing',
-          'attacking_finishing','attacking_heading_accuracy','attacking_short_passing','attacking_volleys','skill_dribbling',
-          'skill_curve','skill_fk_accuracy','skill_long_passing','skill_ball_control','movement_acceleration',
-          'movement_sprint_speed','movement_agility','movement_reactions','movement_balance','power_shot_power',
-          'power_jumping','power_stamina','power_strength','power_long_shots','mentality_aggression','mentality_interceptions',
-          'mentality_positioning','mentality_vision','mentality_penalties','mentality_composure','defending_marking',
-          'defending_standing_tackle','defending_sliding_tackle','goalkeeping_diving','goalkeeping_handling','goalkeeping_kicking',
-          'goalkeeping_positioning','goalkeeping_reflexes']
+    linear_regression = LinearRegression()
+    linear_regression.fit(X_train, y_train)
+    # models.loc['train_mse','MLR'] = mean_squared_error(y_pred=gbr.predict(X_train), y_true=y_train)
+    # models.loc['test_mse','MLR'] = mean_squared_error(y_pred=gbr.predict(X_test), y_true=y_test)
+    head_set=['passing', 'dribbling', 'movement_reactions', 'power_shot_power', 
+    'mentality_composure', 'attacking_short_passing', 'skill_long_passing', 
+    'shooting', 'goalkeeping_diving', 'mentality_vision'] #head
     #input_set=[32,170,72,94,4,4,88,95,70,92,88,97,93,94,92,96,91,84,93,95,95,86,68,75,68,94,48,40,94,94,75,96,33,37,26,6,11,15,14,8]
     new_data = collections.OrderedDict()
     i=0
@@ -217,9 +153,13 @@ def player_prediction2(training_file,input_set):
         new_data[h]=input_set[i]
         i =i+1
     new_data = pd.Series(new_data).values.reshape(1,-1)
-    # print(linear_regression.predict(new_data))
-    return ridge_regression.predict(new_data)[0]
+    # print(gbr.predict(new_data))
+    # return linear_regression.predict(new_data)[0]
+    result = linear_regression.predict([[67, 72, 69, 75, 65, 70, 73, 78, 7, 67]])
+    print("overall:", result)
+    return result
     print('Done')
+
 
 
 #input_set=[32,170,72,4,4,88,95,70,92,88,97,93,94,92,96,91,84,93,95,95,86,68,75,68,94,48,40,94,94,75,96,33,37,26,6,11,15,14,8]
--- project/myapp/views.py
+++ project/myapp/views.py
@@ -640,63 +640,28 @@ from project.settings import BASE_DIR
 def club_query_add(request):
     if request.method == 'POST':
 
-        age = request.POST.get('age')
-        height_cm = request.POST.get('height_cm')
-        weight_kg = request.POST.get('weight_kg')
-        #overall= request.POST.get('overall')
-        player_positions = request.POST.get('player_positions')
-        weak_foot = request.POST.get('weak_foot')
-        skill_moves = request.POST.get('skill_moves')
-        attacking_crossing = request.POST.get('attacking_crossing')
-        attacking_finishing = request.POST.get('attacking_finishing')
-        attacking_heading_accuracy = request.POST.get('attacking_heading_accuracy')
+        
+        passing = request.POST.get('passing')
+        shooting = request.POST.get('shooting')
         attacking_short_passing = request.POST.get('attacking_short_passing')
-        attacking_volleys = request.POST.get('attacking_volleys')
         skill_dribbling = request.POST.get('skill_dribbling')
-        skill_curve = request.POST.get('skill_curve')
-        skill_fk_accuracy = request.POST.get('skill_fk_accuracy')
         skill_long_passing = request.POST.get('skill_long_passing')
-        skill_ball_control = request.POST.get('skill_ball_control')
-        movement_acceleration = request.POST.get('movement_acceleration')
-        movement_sprint_speed = request.POST.get('movement_sprint_speed')
-        movement_agility = request.POST.get('movement_agility')
         movement_reactions = request.POST.get('movement_reactions')
-        movement_balance = request.POST.get('movement_balance')
         power_shot_power = request.POST.get('power_shot_power')
-        power_jumping = request.POST.get('power_jumping')
-        power_stamina = request.POST.get('power_stamina')
-        power_strength = request.POST.get('power_strength')
-        power_long_shots = request.POST.get('power_long_shots')
-        mentality_aggression = request.POST.get('mentality_aggression')
-        mentality_interceptions = request.POST.get('mentality_interceptions')
-        mentality_positioning = request.POST.get('mentality_positioning')
         mentality_vision = request.POST.get('mentality_vision')
-        mentality_penalties = request.POST.get('mentality_penalties')
         mentality_composure = request.POST.get('mentality_composure')
-        defending_marking = request.POST.get('defending_marking')
-        defending_standing_tackle = request.POST.get('defending_standing_tackle')
-        defending_sliding_tackle = request.POST.get('defending_sliding_tackle')
         goalkeeping_diving = request.POST.get('goalkeeping_diving')
-        goalkeeping_handling = request.POST.get('goalkeeping_handling')
-        goalkeeping_kicking = request.POST.get('goalkeeping_kicking')
         goalkeeping_positioning = request.POST.get('goalkeeping_positioning')
-        goalkeeping_reflexes = request.POST.get('goalkeeping_reflexes')
         ###################################################################
 
-        input_set = [float(age),float(height_cm),float(weight_kg),float(weak_foot),
-                        float(skill_moves),float(attacking_crossing),float(attacking_finishing),
-                        float(attacking_heading_accuracy),float(attacking_short_passing),
-                        float(attacking_volleys),float(skill_dribbling),float(skill_curve),
-                        float(skill_fk_accuracy),float(skill_long_passing),float(skill_ball_control),
-                        float(movement_acceleration),float(movement_sprint_speed),float(movement_agility),
-                        float(movement_reactions),float(movement_balance),float(power_shot_power),
-                        float(power_jumping),float(power_stamina),float(power_strength),float(power_long_shots),
-                        float(mentality_aggression),float(mentality_interceptions),float(mentality_positioning),
-                        float(mentality_vision),float(mentality_penalties),float(mentality_composure),float(defending_marking),
-                        float(defending_standing_tackle),float(defending_sliding_tackle),float(goalkeeping_diving),
-                        float(goalkeeping_handling),float(goalkeeping_kicking),float(goalkeeping_positioning),
-                        float(goalkeeping_reflexes)]
-        data_file_path = os.path.join(BASE_DIR, 'data/data.csv')
+        input_set = [
+                    float(passing), float(shooting),  float(power_shot_power),
+                    float(attacking_short_passing), float(skill_dribbling), 
+                    float(skill_long_passing), float(movement_reactions),
+                    float(mentality_vision), float(mentality_composure),
+                    float(goalkeeping_diving), float(goalkeeping_positioning),
+                ]
+        data_file_path = os.path.join(BASE_DIR, 'data/output.csv')
         tr_file = data_file_path
         result = player_test.player_prediction(training_file=tr_file, input_set=input_set)
         overall = player_test.player_prediction2(training_file=tr_file, input_set=input_set)
@@ -709,25 +674,19 @@ def club_query_add(request):
         tm = datetime.today().strftime('%H:%M:%S')
 
         user_id = request.session['user_id']
-        ud = user_query(user_id=user_id, age=age,goalkeeping_handling=goalkeeping_handling,
-                        overall=overall,weight_kg=weight_kg,height_cm=height_cm,
-                        skill_moves=skill_moves,weak_foot=weak_foot,player_positions=player_positions,
-                        attacking_finishing=attacking_finishing,attacking_crossing=attacking_crossing,
-                        attacking_heading_accuracy=attacking_heading_accuracy,
+        ud = user_query(user_id=user_id, 
+                        overall=overall,
                         attacking_short_passing=attacking_short_passing,
-                        skill_dribbling=skill_dribbling,attacking_volleys=attacking_volleys,
-                        skill_fk_accuracy=skill_fk_accuracy,skill_curve=skill_curve,
-                        skill_ball_control=skill_ball_control,skill_long_passing=skill_long_passing,
-                        movement_sprint_speed=movement_sprint_speed,movement_acceleration=movement_acceleration,
-                        movement_reactions=movement_reactions,movement_agility=movement_agility,
-                        power_long_shots=power_long_shots,power_strength=power_strength,power_shot_power=power_shot_power,
-                        power_stamina=power_stamina,power_jumping=power_jumping,movement_balance=movement_balance,
-                        mentality_interceptions=mentality_interceptions,mentality_aggression=mentality_aggression,
-                        mentality_vision=mentality_vision,mentality_positioning=mentality_positioning,
-                        goalkeeping_diving=goalkeeping_diving,defending_sliding_tackle=defending_sliding_tackle,
-                        defending_standing_tackle=defending_standing_tackle,defending_marking=defending_marking,
-                        mentality_composure=mentality_composure,mentality_penalties=mentality_penalties,
-                        goalkeeping_kicking=goalkeeping_kicking,goalkeeping_positioning=goalkeeping_positioning, goalkeeping_reflexes=goalkeeping_reflexes, dt=dt, tm=tm, estimate=estimate)
+                        skill_dribbling=skill_dribbling,
+                       
+                        skill_long_passing=skill_long_passing,
+                        movement_reactions=movement_reactions,
+                        power_shot_power=power_shot_power,
+                        mentality_vision=mentality_vision,
+                        goalkeeping_diving=goalkeeping_diving,
+                        mentality_composure=mentality_composure,
+                        goalkeeping_positioning=goalkeeping_positioning,
+                        dt=dt, tm=tm, estimate=estimate)
         ud.save()
 
         context = {'msg': 'Player avg value estimated','estimate':estimate,'msg2': 'Player overall value estimated','overall':overall}
@@ -758,7 +717,12 @@ from .models import club_games
 def club_games_add(request):
     if request.method == 'POST':
         user_id = request.session['user_id']
-        club_id = int(user_id)
+        club_id = club_master.objects.filter(user_id=user_id)
+        if club_id:
+            club_id = club_id.get()
+        else:
+            return redirect(club_games_add)    
+
         game_name = request.POST.get('game_name')
         game_sub_name = request.POST.get('game_sub_name')
         venue_details =request.POST.get('venue_details')
@@ -768,7 +732,7 @@ def club_games_add(request):
         result = request.POST.get('result')
 
 
-        cg = club_games(club_id=int(club_id),game_name=game_name,game_sub_name=game_sub_name,venue_details=venue_details,
+        cg = club_games(club_id=club_id.id,game_name=game_name,game_sub_name=game_sub_name,venue_details=venue_details,
                          tm=tm,status=status,result=result,dt=dt)
         cg.save()
         context = {'msg': 'Record Added'}
@@ -794,7 +758,11 @@ def club_games_delete(request):
 def club_games_view(request):
     msg = ''
     user_id = request.session['user_id']
-    club_id = int(user_id)
+    club_id = club_master.objects.filter(user_id=user_id)
+    if club_id:
+        club_id = club_id.get()
+    else:
+        return redirect(club_login) 
     cg_l = club_games.objects.filter(club_id=club_id)
     context = {'match_list': cg_l, 'msg': msg}
     return render(request, './myapp/club_games_view.html', context)
